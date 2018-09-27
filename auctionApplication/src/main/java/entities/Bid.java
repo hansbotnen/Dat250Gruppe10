e8d1b3ad60c227ea76cb93309ff4ff46ae25a70c
Philip Hoang <philip.t.hoang@outlook.com>
2018-09-27 12:31:54 +0200

    Some additions in Bid.java

--- auctionApplication/src/main/java/entities/Bid.java
+++ auctionApplication/src/main/java/entities/Bid.java
@@ -13,11 +13,31 @@ public class Bid {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id; 
 	
+	private int bidAmount; 
+	
+	@ManyToOne
+	private Product products;
+	
+	@OneToOne
+	private Account account; 
+	
 	public Bid() {}
 	
 	public int getId() {
 		return id;
 	}
 	
-
+	public int getBidAmount() {
+		return bidAmount;
+	}
+	
+	public void setBidAmount() {
+		this.bidAmount = bidAmount;
+	}
+	
+	
+	public int getAccountId() {
+		return account.getId();
+	}
+	
 }

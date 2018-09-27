05c11c211b4394c761d84aa193eae5e5968a71f8
Philip Hoang <philip.t.hoang@outlook.com>
2018-09-27 17:22:59 +0200

    NOW able to connect to PostgreSQLServer

--- auctionApplication/src/main/java/rest/RestService.java
+++ auctionApplication/src/main/java/rest/RestService.java
@@ -25,45 +25,25 @@ import entities.User;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-@Path("products")
+@Path("rest/products")
 @Stateless
 public class RestService {
-	
-	/*
-	 * TODO: 
-	 * 	GET: products/{id}/bids
-	 * 	GET: products/{pid}
-	 * 
-	 * DOING:
-	 * 	POST: product/{id}/bids
-	 * 
-	 * DONE:
-	 * 	GET: products
-	 * 	GET: products/{id}
-	 * 
-	 * 
-	 * Must also support both XML and JSOM formats as representation
-	 */
 	
 	@Context
 	private UriInfo uriInfo;
 	@PersistenceContext(unitName = "auctionApplication")
 	private EntityManager em;
-	
-	//<app-path>/rest/products
+	/*TODO
 	@GET
-	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-	public Response getProducts() {
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getProduct() {
 		TypedQuery<Product> query = em.createNamedQuery(Product.FIND_ALL, Product.class);
-		
-		List<Product> products = query.getResultList();
-		return Response.ok(products).build();
-	}
+		List<Product> product = query.getResultList();
+		return Response.ok(product).build();
+	}*/
 	
-	//<app-path>/res/products/{id}
 	@GET
 	@Path("{id}")
-	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	public Response getProduct(@PathParam("id") String id) {
 		int idInt = Integer.parseInt(id);
 		Product product = em.find(Product.class, idInt);
@@ -72,11 +52,8 @@ public class RestService {
 		return Response.ok(product).build();
 	}
 	
-	//<app-path>/res/products/{id}/bids
-	//Denne henter kun en ting i bid atm
 	@GET
 	@Path("{id}/bids")
-	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	public Response getBid(@PathParam("id") String id) {
 		int idInt = Integer.parseInt(id);
 		Bid bid = em.find(Bid.class, idInt);
@@ -84,27 +61,16 @@ public class RestService {
 			throw new NotFoundException();
 		return Response.ok(bid).build();
 	} 
-	
 	/*TODO
-	//<app-path>/res/products/{aid}/bids/{bid}
-	pid = product id 
-	@GET
-	@PATH("product/{pid}/bids{bid})
-	public Response getBidFromProduct() {
-	
-	}
-	 */
-	
-	//<app-path>/res/products/{id}/bids
 	@POST
-	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Consumes(MediaType.APPLICATION_XML)
 	@Path("{id}/bids")
 	public Response createBid(Bid bid) {
 		if (bid == null)
 			throw new BadRequestException();
 		em.persist(bid);
-		URI bidUri = uriInfo.getAbsolutePathBuilder().path(Integer.toString(bid.getId())).build();
+		URI bidUri = uriInfo.getAbsolutePathBuilder().path(bid.getId).build();
 		return Response.created(bidUri).build();
-	}
+	}*/
 	
 }

c0b7bca08093f011bb7f75d7cccb3a6f90b6f05c
Philip Hoang <philip.t.hoang@outlook.com>
2018-10-09 15:17:15 +0200

    Fix in REST (duplicates of getAccount())

--- auctionApplication/src/main/java/rest/RestService.java
+++ auctionApplication/src/main/java/rest/RestService.java
@@ -71,24 +71,6 @@ public class RestService {
 	 *				RESTservices for Account				*
 	 *														*
 	 ********************************************************/
-
-	@GET
-	@Path("/accounts")
-	@Produces(MediaType.APPLICATION_XML)
-	public Accounts getAccountsXML() {
-		TypedQuery<Account> query = em.createNamedQuery(Account.FIND_ALL, Account.class);
-		Accounts accounts = new Accounts(query.getResultList());
-		return accounts;
-	}
-	
-	@GET
-	@Path("/accounts/json")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Accounts getAccountsJSON() {
-		TypedQuery<Account> query = em.createNamedQuery(Account.FIND_ALL, Account.class);
-		Accounts accounts = new Accounts(query.getResultList());
-		return accounts;
-	}
 	
 	@POST
 	@Path("/accounts")

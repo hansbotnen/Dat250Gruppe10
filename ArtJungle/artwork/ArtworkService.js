07f6762c6ef20b16dc302f4dd2c4c079e58a842a
Hans Ivar Botnen <hansibotnen@gmail.com>
2018-10-30 18:44:24 +0100

    updating by id

--- ArtJungle/artwork/ArtworkService.js
+++ ArtJungle/artwork/ArtworkService.js
@@ -1,8 +1,9 @@
 const artwork = require('./artwork');
+const createId = require('../utils/idUtil');
 
 class ArtworkService {
   constructor() {
-    this.artworks = [new artwork(1,'Skrik','Munch'), new artwork(2, 'Listhaug', 'AFK')];
+    this.artworks = [new artwork(createId(),'Skrik','Munch'), new artwork(createId(), 'Listhaug', 'AFK')];
   }
 
   getAll() {
@@ -13,10 +14,23 @@ class ArtworkService {
     return this.artworks.find(obj => obj.id == reqId);
   }
 
-  createArtwork(name, artist){
-    var newArtwork = new artwork(this.artworks.length+1, name, artist);
+  createArtwork(name, artist) {
+    var newArtwork = new artwork(createId(), name, artist);
     this.artworks.push(newArtwork);
     return newArtwork;
+  }
+
+  deleteById(reqId) {
+    var deleted = this.getById(reqId);
+    this.artworks = this.artworks.filter(obj => obj.id != reqId);
+    return deleted;
+  }
+
+  updateById(reqId, req) {
+    var artwork = this.deleteById(reqId);
+    artwork = Object.assign({}, artwork, req);
+    this.artworks.push(artwork);
+    return artwork;
   }
 }
 module.exports = new ArtworkService();
--- ArtJungle/artwork/artworkRouter.js
+++ ArtJungle/artwork/artworkRouter.js
@@ -3,31 +3,40 @@ const artwork = require('./artwork');
 const artworkService = require('./ArtworkService');
 const artworkRouter = express.Router();
 
-// Get all artworks
-artworkRouter.get('/', (req, res) => {
-  console.log('Fetching all artworks');
-  res.json(artworkService.getAll());
-});
 
-// Add new artwork
-artworkRouter.post('/', (req, res) => {
-  var name = req.body.name;
-  var artist = req.body.artist;
-  if(name == undefined || artist == undefined) {
-    console.log('Cannot add artwork with name: ' + name + ', artist: ' + artist);
-    res.json();
-  }
-  else{
-    console.log('Adding artwork with name: ' + name + ', artist: ' + artist);
-    res.json(artworkService.createArtwork(name,artist));
-  }
-});
+artworkRouter.route('/')
+  .get((req, res) => {
+    console.log('Fetching all artworks');
+    res.json(artworkService.getAll());
+  })
+  .post((req, res) =>{
+    const name = req.body.name;
+    const artist = req.body.artist;
+    if(name == undefined || artist == undefined) {
+      console.log('Cannot add artwork with name: ' + name + ', artist: ' + artist);
+      res.json();
+    }
+    else{
+      console.log('Adding artwork with name: ' + name + ', artist: ' + artist);
+      res.json(artworkService.createArtwork(name,artist));
+    }
+  })
 
-// Get artwork from ID
-artworkRouter.get('/:artworkId', (req,res) => {
-  const artworkId = req.params.artworkId;
-  console.log('Fetching artwork with id: ' + artworkId);
-  res.json(artworkService.getById(artworkId));
-});
+artworkRouter.route('/:artworkId')
+  .get((req, res) => {
+    const artworkId = req.params.artworkId;
+    console.log('Fetching artwork with id: ' + artworkId);
+    res.json(artworkService.getById(artworkId));
+  })
+  .delete((req, res) => {
+    const artworkId = req.params.artworkId;
+    console.log('Deleting artwork with id: ' + artworkId);
+    res.json(artworkService.deleteById(artworkId));
+  })
+  .put((req, res) => {
+    const artworkId = req.params.artworkId;
+    console.log('Updating artwork with id: ' + artworkId);
+    res.json(artworkService.updateById(artworkId, req.body));
+  })
 
 module.exports = artworkRouter;

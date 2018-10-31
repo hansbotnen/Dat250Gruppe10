1a3df62bf12bbf97360e648b5214245a3051a6e7
Hans Ivar Botnen <hansibotnen@gmail.com>
2018-10-31 01:23:00 +0100

    small fix

--- ArtJungle/utils/dummyDataUtil.js
+++ ArtJungle/utils/dummyDataUtil.js
@@ -1,18 +1,20 @@
 const account = require('../account/account');
 const artwork = require('../artwork/artwork');
 const bid = require('../bid/bid');
-const createId = require('./idUtil');
+
 
 const accountDummies = [
                         new account('2721','Olav Thon'),
-                        new account('a75b', 'Mikal Fuglestein')
+                        new account('a75b', 'Mikkel Mikkelsen')
                        ];
+
 const artworkDummies = [
-                        new artwork(createId(),'Skrik','Munch', '2721'),
+                        new artwork('y625','Skrik','Munch', '2721'),
                         new artwork('701a', 'Listhaug', 'AFK', '2721')
                        ];
+
 const bidDummies = [
-                    new bid(createId(), 'a75b','701a',3.50)
+                    new bid('5352', 'a75b','701a',1000)
                    ];
 
 module.exports = {
